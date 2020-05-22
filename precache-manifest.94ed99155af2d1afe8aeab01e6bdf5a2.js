dd0ac3b178bfa8c903d0cce0f27609722e1eface
JoseLRiano <joselriano@yahoo.com>
2020-05-22 03:51:22 -0400

    Updates

---
+++ precache-manifest.94ed99155af2d1afe8aeab01e6bdf5a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2745325ea313d0ade4ff046b7701e72",
+    "url": "/ReactShoppingList/index.html"
+  },
+  {
+    "revision": "3b182e8980cea109527a",
+    "url": "/ReactShoppingList/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c55a4b68877afa32aa00",
+    "url": "/ReactShoppingList/static/js/2.dee5a43d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactShoppingList/static/js/2.dee5a43d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b182e8980cea109527a",
+    "url": "/ReactShoppingList/static/js/main.743e19b4.chunk.js"
+  },
+  {
+    "revision": "ca1550cb8b0139c8c39f",
+    "url": "/ReactShoppingList/static/js/runtime-main.4fa6859e.js"
+  }
+]);
